dc1fc52a4f2c9c0ccf09c68b57ac37b6d9743caf
unknown <luiz-augustosouza@hotmail.com>
2024-09-10 18:08:41 +0100

    Estilizando a pagina com CSS.

---
+++ src/components/StartScreen.js
@@ -0,0 +1,11 @@
+import "../components/StartScreen.css";
+
+export const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>StartScreen</h1>
+      <p>Clique no botão abaixo para começar a jogar</p>
+      <button>Começar o jogo</button>
+    </div>
+  );
+};
