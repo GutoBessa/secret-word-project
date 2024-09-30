dfe63ed14f4d66f6033778790fd5c2e9accedd8a
unknown <luiz-augustosouza@hotmail.com>
2024-09-30 08:14:39 +0100

    Consumindo states do jogo.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,15 @@
 import "../components/Game.css";
 
-const Game = ({ verifyLetter }) => {
+const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  wrongLetters,
+  guessedLetters,
+  guesses,
+  score,
+}) => {
   return (
     <div className="game">
       <p className="point">
@@ -9,11 +18,19 @@ const Game = ({ verifyLetter }) => {
       <h1>Adivinhe a palavra:</h1>
       <h3 className="tip">
         Dica sobre a palavra:
-        <span>Dica...</span>
+        <span> {pickedCategory} </span>
+        <p>Você ainda tem {guesses} tentativa(s) </p>
       </h3>
       <div className="wordContainer">
-        <span className="letter">A</span>
-        <span className="blankSquare"></span>
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        )}
       </div>
       <div className="letterContainer">
         <p>Tente adivinhar uma outra palavra:</p>
@@ -24,8 +41,9 @@ const Game = ({ verifyLetter }) => {
       </div>
       <div className="wrongLettersContainer">
         <p>Letras ja utilizadas:</p>
-        <span>A,</span>
-        <span>B,</span>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter}, </span>
+        ))}
       </div>
     </div>
   );
